83fdabff8d7fcbe331edcc56b80095d6262027d0
drgg22 <74790257+drgg22@users.noreply.github.com>
2021-06-27 10:37:40 -0300

    update

---
+++ siteT/vs.php
@@ -0,0 +1,21 @@
+<?php
+
+$name;
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head >
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+</head>
+<body style="width: 100%; height: 100%;">
+    <textarea name="" id="Nome" cols="30" rows="10"></textarea>
+    <div style="width:70%; height:80%; margin:auto; outline:solid 2px darkgrey;"></div>
+</body>
+</html>
